6071fcc867cc52a1dd99b43250452480516c64a2
ndmanvar <ndmanvar@yahoo.com>
2016-10-11 13:50:15 -0700

    using https and remove relay stuff

--- util/helpers.js
+++ util/helpers.js
@@ -1,7 +1,7 @@
 var test = require('selenium-webdriver/testing'),
     webdriver = require('selenium-webdriver'),
     SauceLabs = require("saucelabs");
-    
+
 var username,
     accessKey,
     saucelabs,
@@ -10,12 +10,10 @@ var username,
     buildTag,
     sauceSeUri,
     tunnelId;
-    
+
 function setSauceEnv(){
     username = process.env.SAUCE_USERNAME;
     accessKey = process.env.SAUCE_ACCESS_KEY;
-    seRelayHost = process.env.SELENIUM_HOST;
-    seRelayPort = process.env.SELENIUM_PORT;
     buildTag = process.env.BUILD_TAG;
     tunnelId = process.env.TUNNEL_IDENTIFIER;
     //making sure we have some username and access key
@@ -23,15 +21,7 @@ function setSauceEnv(){
         console.error("Sauce username and password is not defined!");
         process.exit(1);
     }
-    //checking if we are running in a CI environment with SC 
-    //(SC running within the Sauce CI plugin) enabled.
-    //If it is, then we use the defined SC relay, if not we connect
-    //directly
-    if (seRelayHost == undefined || seRelayPort == undefined){
-        seRelayHost = "ondemand.saucelabs.com";
-        seRelayPort = "80";
-    }
-    
+
     saucelabs = new SauceLabs({
         username: username,
         password: accessKey
@@ -42,9 +32,9 @@ function beforeEachExample() {
     var browser = process.env.BROWSER,
         version = process.env.VERSION,
         platform = process.env.PLATFORM,
-        server = "http://" + username + ":" + accessKey +
-        "@" + seRelayHost + ":" + seRelayPort + "/wd/hub";
-        
+        server = "https://" + username + ":" + accessKey +
+        "@ondemand.saucelabs.com:443/wd/hub";
+
     var desiredCaps = {
         'browserName': browser,
         'platform': platform,
@@ -55,7 +45,7 @@ function beforeEachExample() {
         };
     //check if buildTag is set if so add to desired caps.
     if (buildTag != undefined){
-        desiredCaps['build'] = buildTag; 
+        desiredCaps['build'] = buildTag;
     }
     //check if there's a tunnel identifier set by CI (Plugin)
     if (tunnelId != undefined){
@@ -73,7 +63,7 @@ function beforeEachExample() {
 
 function afterEachExample(done) {
 	var passed = (this.currentTest.state === 'passed') ? true : false;
-    
+
     saucelabs.updateJob(driver.sessionID, {
       passed: passed
     }, done);
